1ec69d825e4b0ab921e7f2e1943ed5a274d6fe45
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2023-12-16 18:20:18 +0530

    Design Profile Page

---
+++ src/components/Profile/ProfilePost.jsx
@@ -0,0 +1,167 @@
+import {
+  Avatar,
+  Box,
+  Divider,
+  Flex,
+  GridItem,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  Text,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { AiFillHeart } from "react-icons/ai";
+import { FaComment } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import Comment from "../Comment/Comment";
+import PostFooter from "../FeedPosts/PostFooter";
+
+const ProfilePost = ({ img }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <GridItem
+        cursor={"pointer"}
+        borderRadius={4}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"whiteAlpha.300"}
+        position={"relative"}
+        aspectRatio={1 / 1}
+        onClick={onOpen}
+      >
+        <Flex
+          opacity={0}
+          _hover={{ opacity: 1 }}
+          position={"absolute"}
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bg={"blackAlpha.700"}
+          transition={"all 0.3s ease"}
+          zIndex={1}
+          justifyContent={"center"}
+        >
+          <Flex alignItems={"center"} justifyContent={"center"} gap={50}>
+            <Flex>
+              <AiFillHeart size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
+            <Flex>
+              <FaComment size={20} />
+              <Text fontWeight={"bold"} ml={2}>
+                7
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
+
+        <Image
+          src={img}
+          alt="profile post"
+          w={"100%"}
+          h={"100%"}
+          objectFit={"cover"}
+        />
+      </GridItem>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered={true}
+        size={{ base: "3xl", md: "5xl" }}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody bg={"black"} pb={5}>
+            <Flex
+              gap={4}
+              w={{ base: "90%", sm: "70%", md: "full" }}
+              mx={"auto"}
+            >
+              <Box
+                borderRadius={4}
+                overflow={"hidden"}
+                border={"1px solid"}
+                borderColor={"whiteAlpha.300"}
+                flex={1.5}
+                margin={"auto"}
+              >
+                <Image src={img} alt="profile post" />
+              </Box>
+              <Flex
+                flex={1}
+                flexDir={"column"}
+                px={10}
+                display={{ base: "none", md: "flex" }}
+              >
+                <Flex alignItems={"center"} justifyContent={"space-between"}>
+                  <Flex alignItems={"center"} gap={4}>
+                    <Avatar src="/profilepic.png" size={"sm"} name="asha" />
+                    <Text fontWeight={"bold"} fontSize={12}>
+                      asha
+                    </Text>
+                  </Flex>
+                  <Box
+                    _hover={{ bg: "whiteAlpha.300", color: "red.600" }}
+                    borderRadius={4}
+                    p={1}
+                  >
+                    <MdDelete size={20} cursor={"pointer"} />
+                  </Box>
+                </Flex>
+                <Divider my={4} bg={"gray.500"} />
+
+                <VStack
+                  w="full"
+                  alignItems={"start"}
+                  maxH={"350px"}
+                  overflowY={"auto"}
+                >
+                  <Comment
+                    createdAt="1d ago"
+                    username="yuta"
+                    profilePic="https://i.pinimg.com/736x/06/be/d8/06bed8da894432ded576ef581edd04a5.jpg"
+                    text={"GEGE! when I find you GEGEE!!"}
+                  />
+                  <Comment
+                    createdAt="1h ago"
+                    username="panda"
+                    profilePic="https://i.pinimg.com/736x/f0/a7/4d/f0a74df6eb66ebb33743e80beec91a1c.jpg"
+                    text={"panda janai"}
+                  />
+                  <Comment
+                    createdAt="12h ago"
+                    username="inumaki"
+                    profilePic="https://i.pinimg.com/originals/01/3f/31/013f3192d737ded2d8dace7842a25a7b.jpg"
+                    text={"tuna mayo"}
+                  />
+                  <Comment
+                    createdAt="12h ago"
+                    username="inumaki"
+                    profilePic="https://i.pinimg.com/originals/01/3f/31/013f3192d737ded2d8dace7842a25a7b.jpg"
+                    text={"しお"}
+                  />
+                </VStack>
+                <Divider my={4} bg={"gray.800"} />
+
+                <PostFooter isProfilePage={true} />
+              </Flex>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ProfilePost;
