47dc3442dd53717a584d0e3d4df2e64803e2a391
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-01-01 23:52:09 +0530

    Google Authentication

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -1,12 +1,57 @@
 import { Flex, Image, Text } from "@chakra-ui/react";
+import { auth, firestore } from "../../firebase/firebase";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import useShowToast from "../../hooks/useShowToast";
+import useAuthStore from "../../store/authStore";
+import { doc, setDoc } from "firebase/firestore";
 
-const GoogleAuth = () => {
+const GoogleAuth = ({ prefix }) => {
+  const [signInWithGoogle, , , error] = useSignInWithGoogle(auth);
+  const showToast = useShowToast();
+  const loginUser = useAuthStore((state) => state.login);
+
+  const handleGoogleAuth = async () => {
+    try {
+      const newUser = await signInWithGoogle();
+      if (!newUser && error) {
+        showToast("Error", error.message, "error");
+        return;
+      }
+
+      // nanami@gmail.com
+      if (newUser) {
+        const userDoc = {
+          uid: newUser.user.uid,
+          email: newUser.user.email,
+          username: newUser.user.email.split("@")[0], //nanami
+          fullName: newUser.user.displayName,
+          bio: "",
+          profilePicURL: newUser.user.photoURL,
+          followers: [],
+          following: [],
+          posts: [],
+          createdAt: Date.now(),
+        };
+        await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+        localStorage.setItem("user-info", JSON.stringify(userDoc));
+        loginUser(userDoc);
+      }
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    }
+  };
+
   return (
     <>
-      <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
+      <Flex
+        alignItems={"center"}
+        justifyContent={"center"}
+        cursor={"pointer"}
+        onClick={handleGoogleAuth}
+      >
         <Image src="/google.png" w={5} alt="Google logo" />
         <Text mx={2} color={"blue.500"}>
-          Log in with Google
+          {prefix} with Google
         </Text>
       </Flex>
     </>
