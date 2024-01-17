4f59c718b8ba4e7ffae44d6a217597a05f16eec0
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2024-01-17 20:54:40 +0530

    Fetch Feed Posts

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -4,53 +4,46 @@ import {
   Flex,
   Skeleton,
   SkeletonCircle,
+  Text,
   VStack,
 } from "@chakra-ui/react";
 import FeedPost from "./FeedPost";
-import { useEffect, useState } from "react";
+import useGetFeedPosts from "../../hooks/useGetFeedPosts";
 
 const FeedPosts = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []);
+  const { isLoading, posts } = useGetFeedPosts();
 
   return (
     <Container maxW={"container.sm"} py={10} px={2}>
       {isLoading &&
-        [0, 1, 2, 3, 4].map((_, idx) => (
+        [0, 1, 2].map((_, idx) => (
           <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
-            <Flex gap={2}>
+            <Flex gap="2">
               <SkeletonCircle size="10" />
-              <VStack>
+              <VStack gap={2} alignItems={"flex-start"}>
                 <Skeleton height="10px" w={"200px"} />
                 <Skeleton height="10px" w={"200px"} />
               </VStack>
             </Flex>
             <Skeleton w={"full"}>
-              <Box h={"500px"}>content wrapped</Box>
+              <Box h={"400px"}>contents wrapped</Box>
             </Skeleton>
           </VStack>
         ))}
 
-      {!isLoading && (
+      {!isLoading &&
+        posts.length > 0 &&
+        posts.map((post) => <FeedPost key={post.id} post={post} />)}
+
+      {!isLoading && posts.length === 0 && (
         <>
-          <FeedPost img="maki.jpg" username="maki" avatar="maki.jpg" />
-          <FeedPost
-            img="/strongest.png"
-            username="gojo"
-            avatar="/jjk_trio1.png"
-          />
-          <FeedPost img="/nobara.png" username="nobara" avatar="/nobara.png" />
-          <FeedPost img="/gumi.jpg" username="megumi" avatar="/gumi.jpg" />
-          <FeedPost
-            img="special_grade_sqaud.jpg"
-            username="yuji"
-            avatar="jjk_trio2.jpg"
-          />
+          <Text fontSize={"md"} color={"red.400"}>
+            Whoa, friend count looking a bit lean, huh?
+          </Text>
+          <Text color={"red.400"}>
+            Pause the coding marathon and let's turn it into a social spree!
+            🚀🤝
+          </Text>
         </>
       )}
     </Container>
--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -16,8 +16,9 @@ import {
 import usePostComment from "../../hooks/usePostComment";
 import useAuthStore from "../../store/authStore";
 import useLikePost from "../../hooks/useLikePost";
+import { timeAgo } from "../../utils/timeAgo";
 
-const PostFooter = ({ post, username, isProfilePage }) => {
+const PostFooter = ({ post, isProfilePage, creatorProfile }) => {
   const { isCommenting, handlePostComment } = usePostComment();
   const [comment, setComment] = useState("");
   const authUser = useAuthStore((state) => state.user);
@@ -48,17 +49,25 @@ const PostFooter = ({ post, username, isProfilePage }) => {
         {likes} likes
       </Text>
 
+      {isProfilePage && (
+        <Text fontSize="12" color={"gray"}>
+          Posted {timeAgo(post.createdAt)}
+        </Text>
+      )}
+
       {!isProfilePage && (
         <>
           <Text fontSize={"sm"} fontWeight={700}>
-            {username}{" "}
+            {creatorProfile?.username}{" "}
             <Text as="span" fontWeight={400}>
-              Feeling good
+              {post.caption}
             </Text>
           </Text>
-          <Text fontSize={"sm"} color={"gray"}>
-            View all 1,000 comments
-          </Text>
+          {post.comments.length > 0 && (
+            <Text fontSize={"sm"} color={"gray"} cursor={"pointer"}>
+              View all {post.comments.length} comments
+            </Text>
+          )}
         </>
       )}
 
